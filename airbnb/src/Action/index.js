6353ceeb5c7e9918792f2ed266f240a00853a9cc
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-21 17:39:43 -0400

    Create registration form using a custom hooks and redux

--- airbnb/src/Action/index.js
+++ airbnb/src/Action/index.js
@@ -1,13 +1,44 @@
-// import { axiosWithAuth } from "../utils";
+import axios from "axios";
+import { axiosWithAuth } from "../utils";
 
 // action types
 export const REGISTER_REQUEST = "REGISTER_REQUEST";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAILURE = "REGISTER_FAILURE";
 
+//action creator
+export const newUser = user => dispatch => {
+  console.log(user)
+  //action object
+  dispatch({ type: REGISTER_REQUEST })
+  // do some async action and dispatch an error or success action
+  axios
+    .post(`https://airbnb-optimal.herokuapp.com/api/auth/register`, user)
+    .then(res => {
+      console.log("POST Request Response: ", res)
+      dispatch({ type: REGISTER_SUCCESS, payload: res.data })
+    })
+    .catch(err => dispatch({ type: REGISTER_FAILURE, payload: err.payload }));
+}
 
 
 // action types
 export const LOGIN_REQUEST = "LOGIN_REQUEST";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+//action creator
+export const userLogin = user => dispatch => {
+  console.log(user)
+  //action object
+  dispatch({ type: LOGIN_REQUEST })
+  // do some async action and dispatch an error or success action
+  axiosWithAuth()
+    .post(`https://airbnb-optimal.herokuapp.com/api/auth/login`, user)
+    .then(res => {
+      console.log("POST Request Response: ", res)
+      localStorage.setItem('token', res.data.token);
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data })
+    })
+    .catch(err => dispatch({ type: LOGIN_FAILURE, payload: err.payload }));
+}
--- airbnb/src/Components/Registration.js
+++ airbnb/src/Components/Registration.js
@@ -1,55 +1,63 @@
 import React from "react";
+import Navbar from "./NavBar";
 import useForm from "../Hooks/UseForm";
+import { connect } from "react-redux";
+import { newUser } from "../Action";
+import { Link } from 'react-router-dom';
 
-const Registration = () => {
-  const { values, handleChange, handleSubmit } = useForm(
-    { name: "", lastName: "", email: "", password: "" },
-    login
-  );
 
+const Registration = props => {
+  console.log(props);
+  const { values, handleChange, handleSubmit } = useForm(register);
+
+  //register is being pass as a callback function to my custom hook
   function register() {
     console.log(values);
+    //dispatch function
+    props.newUser(values);
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="input"
-          type="text"
-          name="name"
-          onChange={handleChange}
-          value={values.name}
-          required
-        />
-        <input
-          className="input"
-          type="text"
-          name="lastName"
-          onChange={handleChange}
-          value={values.lastName}
-          required
-        />
-        <input
-          className="input"
-          type="email"
-          name="email"
-          onChange={handleChange}
-          value={values.email}
-          required
-        />
-        <input
-          className="input"
-          type="password"
-          name="password"
-          onChange={handleChange}
-          value={values.password}
-          required
-        />
-        <button type="submit">Registration</button>
-      </form>
+    <Navbar />
+    <div class="form__wrapper">
+      <div class="form__container">
+        <h1>Create an account</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="input"
+            type="text"
+            name="username"
+            onChange={handleChange}
+            value={values.username}
+            placeholder="username"
+            required
+          />
+          <input
+            className="input"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={values.password}
+            placeholder="Password"
+            required
+          />
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+      <p>Already have an account? <Link to="/login">Login here</Link></p>
+    </div>
     </>
   );
 };
 
-export default Registration;
+const mapStateToProps = state => {
+  return {
+    user: state.values
+  };
+};
+// I'm connecting to the redux store
+export default connect(
+  mapStateToProps,
+  { newUser }
+)(Registration);
