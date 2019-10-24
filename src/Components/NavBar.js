e17af548fdf12af34f3adc8c61cd4fcbc9de892d
Aaron-Doran <wesley.gossett@gmail.com>
2019-10-24 15:53:58 -0400

    Trying to style login and register a link to buttons

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from '../images/logo.png'
+import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return ( 
+  return (
     <header>
       <nav>
-        <Link  to="/"><img className="logo" src={logo} alt="logo"/></Link>
-      <ul>
-        <li>
-            <a href="#DATA-DRIVEN">How It Works</a>
-          </li>
+        <Link to='/'>
+          <img className='logo' src={logo} alt='logo' />
+        </Link>
+        <ul>
           <li>
-            <Link to="/login">Login</Link>
+            <a href='#DATA-DRIVEN'>How It Works</a>
           </li>
-          <li>
-            <Link to="/registration">Sign up</Link>
+          <li className='login'>
+            <Link to='/login'>Login</Link>
+          </li>
+          <li className='register'>
+            <Link to='/registration'>Sign up</Link>
           </li>
         </ul>
       </nav>
     </header>
-   );
-}
- 
+  );
+};
+
 export default Navbar;
